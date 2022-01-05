cb2aa47c038832e3ee6e7105a1c31a8efac03bde
rockstar5495 <pratyakshom.05@gmail.com>
2022-01-05 14:52:13 +0000

    mido: build with Neverallows flag * it ignores some sepolicy neverallows to make build proceed
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,9 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# Neverallows
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /vendor/lib64/hw/gxfingerprint.default.so|fakelogprint.so \
