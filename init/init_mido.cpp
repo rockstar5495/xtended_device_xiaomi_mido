37dad4c42e586425db3fec5c81f7e1c0de433ce5
rockstar5495 <pratyakshom.05@gmail.com>
2022-01-06 20:18:54 +0000

    mido: init: drop permissive hacks * its conflicting with SELinux state
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- init/init_mido.cpp
+++ init/init_mido.cpp
@@ -134,8 +134,6 @@ static void workaround_snet_properties() {
         property_override(snet_prop_key[i], snet_prop_value[i]);
     }
 
-     chmod("/sys/fs/selinux/enforce", 0640);
-     chmod("/sys/fs/selinux/policy", 0440);
 }
 
 void vendor_load_properties()
