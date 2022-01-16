018ae1126ae38ff87d603046fa93fd25abe691e2
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-01-16 21:28:51 +0000

    mido: update build fp & desc to raven Jan patch
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- octavi_mido.mk
+++ octavi_mido.mk
@@ -48,7 +48,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
